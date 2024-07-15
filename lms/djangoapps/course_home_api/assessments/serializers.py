eaeb36cd5ccadc50c28da66f04d65f1c14a736c2
Chandana3008 <chandana.k@talentsprint.com>
2024-07-15 15:54:29 +0530

    Debugging

--- lms/djangoapps/course_home_api/assessments/serializers.py
+++ lms/djangoapps/course_home_api/assessments/serializers.py
@@ -131,6 +131,7 @@ class CourseSummary(serializers.Serializer):
         course_name = representation.pop('name')  # Get and remove the course name
 
         merged_subsections = SubsectionScoresSerializerOuter(many=True).data
+        log.info(merged_subsections)
         # Add course name to each date_block
         start_date = ""
         for date_block in representation['date_blocks']:
