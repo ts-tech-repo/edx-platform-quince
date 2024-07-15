71127829f6c7f97f75e3752e9028ade462a01df9
manoj.p3 <manoj.p@talentsprint.com>
2024-07-15 18:08:44 +0530

    Added colab submisson report

--- common/djangoapps/student/views/management.py
+++ common/djangoapps/student/views/management.py
@@ -1355,3 +1355,31 @@ def _get_active_inactive_courses(user):
         else:
             user_active_inactive_courses.update({user_course["course_details"]["course_id"] : "Dropped"})
     return user_active_inactive_courses
+
+@login_required
+def extras_notebook_submissions(request):
+    MEMCACHE_TIMEOUT = 3600
+    token = uuid.uuid4().hex
+    value = request.user.id
+    cache.set(token, value, MEMCACHE_TIMEOUT)
+
+    redirect_url = "https://dashboard.talentsprint.com/submissions/notebook/auth.html?tokenID=" + token + "&requestingDomain="+ configuration_helpers.get_value("SITE_NAME", "maple.talentsprint.com")
+    return redirect(redirect_url)
+    #return JsonResponse({"token" : token})
+
+def extras_userdetails(request):
+    try:
+        if "uid" in request.GET:
+            uid = request.GET["uid"]
+        elif "token" in request.GET:
+            uid = cache.get(request.GET["token"])
+    except MultiValueDictKeyError:
+        return render(request, 'blank.html', {"message": "Key Missing"})
+
+    try:
+        u = User.objects.get(id = uid)
+        p = UserProfile.objects.get(user = u)
+        user_details = {"username": u.username,"name" : u.first_name + " " + u.last_name,"mail" : u.email, "uid" : uid}
+        return JsonResponse(user_details);
+    except User.DoesNotExist:
+        return render(request, 'blank.html', {"message": "User Does Not Exist"})
