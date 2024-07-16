6fcc00ac2d54644a417c613aa96fb039cd8fc303
Chandana3008 <chandana.k@talentsprint.com>
2024-07-16 15:22:40 +0530

    Debugging

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -969,7 +969,8 @@ def get_assessments_for_courses(request):
         block_data = get_course_blocks(user, course_usage_key, allow_start_dates_in_future=True, include_completion=True)
         for section_key in block_data.get_children(course_usage_key):  
             for subsection_key in block_data.get_children(section_key):
-                    start = parse_date(block_data.get_xblock_field(subsection_key, 'start')).replace(tzinfo=pytz.UTC)
+                    start = block_data.extras_get_xblock_field(subsection_key, 'start')
+                    log.info(start)
                     due = block_data.get_xblock_field(subsection_key, 'due')
                     temp = {"course_name" : user_course["course_details"]["course_name"], "title" : block_data.get_xblock_field(subsection_key, 'display_name'), "start_date" : start, "date" : due, "link" : reverse('jump_to', args=[course_key, subsection_key])}
                     try:
