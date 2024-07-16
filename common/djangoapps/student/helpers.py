7c8d823c63f3ac85ed5001921e5778a1a264c62e
Chandana3008 <chandana.k@talentsprint.com>
2024-07-16 11:44:12 +0530

    Debugging

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -988,6 +988,7 @@ def get_assessments_for_courses(request):
                         #     temp["submission_status"] = "Not Submitted"
                         if category == "openassessment":
                             student_module_info = StudentModule.get_state_by_params(course_key_string, [block_id], user.id)
+                            log.info(student_module_info)
                             if not student_module_info:
                                 temp["submission_status"] = "Not Submitted"
 
