afd3e30e59e4bd70478a7161c8bd96a378ff14c4
Chandana3008 <chandana.k@talentsprint.com>
2024-07-16 10:46:11 +0530

    debugging

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -960,10 +960,15 @@ def get_assessments_for_courses(request):
         block_data = get_course_blocks(user, course_usage_key, allow_start_dates_in_future=True, include_completion=True)
         for section_key in block_data.get_children(course_usage_key):  
             for subsection_key in block_data.get_children(section_key):
+                log.info(subsection_key)
                 units = block_data.get_children(subsection_key)
+                
                 while units:
                     unit = units.pop()
-                    log.info(block_data.get_children(unit))
+                    title = block_data.get_xblock_field(unit, 'display_name')
+                    log.info(title)
+                    children = block_data.get_children(unit)
+
         # split_modulestore = modulestore()._get_modulestore_by_type(ModuleStoreEnum.Type.split)
         # active_version_collection = split_modulestore.db_connection.course_index
         # structure_collection = split_modulestore.db_connection.structures
