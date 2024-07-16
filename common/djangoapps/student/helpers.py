27a3cf59676c2110c8af28d90c2e315ff1acdc73
Chandana3008 <chandana.k@talentsprint.com>
2024-07-16 06:45:48 +0530

    Added get_course_assignments function

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -50,7 +50,7 @@ from lms.djangoapps.instructor import access
 from lms.djangoapps.verify_student.models import VerificationDeadline
 from lms.djangoapps.verify_student.services import IDVerificationService
 from lms.djangoapps.verify_student.utils import is_verification_expiring_soon, verification_for_datetime
-from lms.djangoapps.courseware.courses import get_course, get_course_date_blocks, get_course_with_access
+from lms.djangoapps.courseware.courses import get_course, get_course_assignments, get_course_date_blocks, get_course_with_access
 from lms.djangoapps.courseware.date_summary import TodaysDate
 from lms.djangoapps.courseware.context_processor import user_timezone_locale_prefs
 from lms.djangoapps.course_home_api.dates.serializers import DateSummarySerializer
@@ -953,7 +953,7 @@ def get_assessments_for_courses(request):
 
         if CourseEnrollment.is_enrolled(request.user, course_key):
             blocks = get_course_date_blocks(course, request.user, request, include_access=True, include_past_dates=True)
-            log.info(get_course(course_key, 3))
+            log.info(get_course_assignments(course_key, request.user))
             new_blocks = [block for block in blocks if not isinstance(block, TodaysDate)]
             response_data["courses"].append({
                 'name':user_course["course_details"]["course_name"],
