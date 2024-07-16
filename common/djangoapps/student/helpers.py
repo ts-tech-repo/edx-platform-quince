7846dfa38be7e874e55d847fdf2ffcb0649ac970
Chandana3008 <chandana.k@talentsprint.com>
2024-07-16 16:18:44 +0530

    Debugging

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -952,7 +952,7 @@ def get_assessments_for_courses(request):
     user_courses = get_course_enrollments(user.username)
     response_data = {"courses":[]}
     for i, user_course in enumerate(user_courses):
-        
+        log.info(user_courses)
         course_key_string = user_course["course_details"]["course_id"]
         course_key = CourseKey.from_string(course_key_string)
         is_staff = bool(has_access(request.user, 'staff', course_key))
