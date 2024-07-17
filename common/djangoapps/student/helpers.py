908965a1c7bc2eceb0c299453bfc83106d505829
Chandana3008 <chandana.k@talentsprint.com>
2024-07-17 12:12:44 +0530

    Debugging

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -988,9 +988,11 @@ def get_assessments_for_courses(request):
                     
                         components = block_data.get_children(unit)
                         for component in components:
-                            block_id = get_first_component_of_block(component, block_data)
-                            response = handle_xblock_callback(request, course_key_string, block_id, "get_staff_grading_data")
-                            log.info(json.loads(response.content.decode('utf-8')))
+                            category = block_data.get_xblock_field(component, 'category')
+                            if category == "edx-sga":
+                                block_id = get_first_component_of_block(component, block_data)
+                                response = handle_xblock_callback(request, course_key_string, block_id, "get_staff_grading_data")
+                                log.info(json.loads(response.content.decode('utf-8')))
                             student_module_info = StudentModule.get_state_by_params(course_key_string, [block_id], user.id)
                             if not temp.get("submission_status", None):
                                 if not student_module_info:
