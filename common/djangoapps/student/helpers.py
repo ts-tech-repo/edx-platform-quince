e17c7a5db62a95048b65462314c2965d151d3a16
Chandana3008 <chandana.k@talentsprint.com>
2024-07-17 12:40:19 +0530

    Debugging

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -990,7 +990,6 @@ def get_assessments_for_courses(request):
                         for component in components:
                             category = block_data.get_xblock_field(component, 'category')
                             block_id = get_first_component_of_block(component, block_data)
-                            log.info(get_block_by_usage_id(request, course_key_string, block_id))
                             student_module_info = StudentModule.get_state_by_params(course_key_string, [block_id], user.id)
                             if not temp.get("submission_status", None):
                                 if not student_module_info:
