1d562a77eb3f74792c06997b0064d5551665d815
Chandana3008 <chandana.k@talentsprint.com>
2024-07-17 11:31:29 +0530

    Removed function call

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -987,7 +987,6 @@ def get_assessments_for_courses(request):
                     
                         components = block_data.get_children(unit)
                         for component in components:
-                            log.info(get_submission_info(request, unit, "e2a4229d63cb48f8ab41001360fba96f"))
                             block_id = get_first_component_of_block(component, block_data)
                             student_module_info = StudentModule.get_state_by_params(course_key_string, [block_id], user.id)
                             if not temp.get("submission_status", None):
