9bba65711a4f112ac04e454c450edf204e34e32f
Chandana3008 <chandana.k@talentsprint.com>
2024-07-18 10:14:51 +0530

    Debugging

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -1017,7 +1017,7 @@ def get_assessments_for_courses(request):
         
                         
                         
-    filtered_sorted_date_blocks = sorted(all_blocks_data, key=lambda x: x.get('start_date', None))
+    filtered_sorted_date_blocks = sorted(all_blocks_data, key=lambda x: x.get('start_date', None), sortNone = None)
     user_local_timezone = user_timezone_locale_prefs(request)
 
     return {
