8d1610dfc6dfc38d556a328af116f726f17f86c5
Chandana3008 <chandana.k@talentsprint.com>
2024-07-19 10:36:15 +0530

    Debugging

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -961,6 +961,7 @@ def get_assessments_for_courses(request):
             for subsection_key in block_data.get_children(section_key):
                     start = block_data.get_xblock_field(subsection_key, 'start')
                     due = block_data.get_xblock_field(subsection_key, 'due')
+                    log.info(datetime.fromisoformat(due))
                     ignoreUnit = False
                     temp = {"course_name" : user_course["course_details"]["course_name"], "title" : block_data.get_xblock_field(subsection_key, 'display_name'), "start_date" : start, "date" : due, "link" : reverse('jump_to', args=[course_key, subsection_key])}
                     try:
@@ -998,13 +999,13 @@ def get_assessments_for_courses(request):
                                     elif not sga_submissions.answer.get("finalized"):
                                         temp["submission_status"] = "In Progress"
                                 except Exception as err:
-                                    temp["submission_status"] = "Not Submitted" if due and datetime.now() > due else "-"
+                                    temp["submission_status"] = "Not Submitted" if due is not None and datetime.now() > due else "-"
                                     temp["is_graded"] = "-"
                             
                             else:
                                 if not temp.get("submission_status", None):
                                     if not student_module_info:
-                                        temp["submission_status"] = "Not Submitted" if due and datetime.now() > due else "-"
+                                        temp["submission_status"] = "Not Submitted" if due is not None and datetime.now() > due else "-"
                                         temp["is_graded"] = "-"
                                 
                                 if student_module_info:
