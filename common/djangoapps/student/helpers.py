57dc9187eac308a313d26d4dad2ce4f9ef0bdb9c
Chandana3008 <chandana.k@talentsprint.com>
2024-07-19 12:05:12 +0530

    Debugging

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -1016,7 +1016,7 @@ def get_assessments_for_courses(request):
                                     temp["is_graded"] = "-"
 
                             elif category in ["problem"]:
-                                if student_module_info and student_module_info.state and "last_submission_time" in student_module_info:
+                                if student_module_info and student_module_info.state and "last_submission_time" in student_module_info.state:
                                     temp["submission_status"] = "Submitted"
                                     temp["is_graded"] = "Not Graded"
                                 elif ("submission_status" in temp and temp["submission_status"] in ["Not Submitted"]) or not temp.get("submission_status", None):
