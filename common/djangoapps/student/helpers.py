cdc46515f370554cbbe7011b7e3619713557a397
Chandana3008 <chandana.k@talentsprint.com>
2024-08-08 15:12:55 +0530

    Commented grades

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -959,14 +959,14 @@ def get_assessments_for_courses(request):
                 showNotSubmitted = True if due is not None and datetime.now() > due.replace(tzinfo=None) else  False
                 ignoreUnit = False
                 temp = {"course_name" : user_course["course_details"]["course_name"], "title" : block_data.get_xblock_field(subsection_key, 'display_name'), "start_date" : start, "date" : due, "link" : reverse('jump_to', args=[course_key, subsection_key])}
-                try:
-                    grades = PersistentSubsectionGrade.read_grade(user.id, subsection_key)
-                    if grades.first_attempted is not None:
-                        temp["is_graded"] = "Graded"
-                    else:
-                        temp["is_graded"] = "Not Graded"
-                except Exception as DoesNotExistError:
-                    temp["is_graded"] = "Not Graded"
+                # try:
+                #     grades = PersistentSubsectionGrade.read_grade(user.id, subsection_key)
+                #     if grades.first_attempted is not None:
+                #         temp["is_graded"] = "Graded"
+                #     else:
+                #         temp["is_graded"] = "Not Graded"
+                # except Exception as DoesNotExistError:
+                #     temp["is_graded"] = "Not Graded"
                 
                 units = block_data.get_children(subsection_key)
                 if not units:
