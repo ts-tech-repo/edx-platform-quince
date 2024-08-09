74dc8d853fcfdf0361ceccce91bfb6df558ceb95
Chandana3008 <chandana.k@talentsprint.com>
2024-08-09 12:15:08 +0530

    Debgging

--- common/djangoapps/student/views/management.py
+++ common/djangoapps/student/views/management.py
@@ -1693,6 +1693,7 @@ def extras_get_assessment_details(request):
     # limit = int(request.POST.get("limit"))
     course_key = CourseKey.from_string(str(course_id))
     user = User.objects.filter(is_superuser=True, email = "ga-admin@talentsprint.com")
+    log.info(user)
     log.info(get_course_assignments(course_key, user))
     # enrolled_users = enrolled_students_features(course_key, ["id", "username","first_name","last_name","email"])
     # course_usage_key = modulestore().make_course_usage_key(course_key)
