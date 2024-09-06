bb597db10af7546a0406e38f6926e3b83fd813a4
Chandana3008 <chandana.k@talentsprint.com>
2024-09-06 17:46:02 +0530

    Reverted changes

--- openedx/features/course_experience/utils.py
+++ openedx/features/course_experience/utils.py
@@ -221,4 +221,4 @@ def is_block_structure_complete_for_assignments(block_data, block_key, course_ke
         if not ENABLE_COMPLETION_TRACKING_FLAG.is_enabled(course_key):
             return graded or scored
     
-    return complete
+    return complete or not graded or not scored
