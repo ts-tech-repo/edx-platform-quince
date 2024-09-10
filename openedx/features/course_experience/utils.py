6ca36f8998a8b3aa0234f13aef364214f1925103
Chandana3008 <chandana.k@talentsprint.com>
2024-09-10 16:39:34 +0530

    Debuggin

--- openedx/features/course_experience/utils.py
+++ openedx/features/course_experience/utils.py
@@ -229,7 +229,7 @@ def is_block_structure_complete_for_assignments(block_data, block_key, course_ke
     scored = has_score and (weight is None or weight > 0)
     log.info(f"Block {block_key}: complete={complete}, graded={graded}, has_score={has_score}, scored={scored}")
     if course_key:
-        if not ENABLE_COMPLETION_TRACKING_FLAG.is_enabled():
+        if not ENABLE_COMPLETION_TRACKING_FLAG.is_enabled(course_key):
             log.info(ENABLE_COMPLETION_TRACKING_FLAG.is_enabled(course_key))
             return graded or scored
     log.info(complete)
