4841855817377630068ae6d7aa006781b95069e9
Chandana3008 <chandana.k@talentsprint.com>
2024-10-11 16:16:59 +0530

    Debugging

--- lms/djangoapps/courseware/courses.py
+++ lms/djangoapps/courseware/courses.py
@@ -612,6 +612,9 @@ def get_course_assignments(course_key, user, include_access=False, customAPI=Fal
         for subsection_key in block_data.get_children(section_key):
             due = block_data.get_xblock_field(subsection_key, 'due')
             graded = block_data.get_xblock_field(subsection_key, 'graded', False)
+            log.info(block_data.get_xblock_field(subsection_key, 'display_name'))
+            log.info(due)
+            log.info(graded)
             if (due and graded) or (customAPI and graded):
                 first_component_block_id = get_first_component_of_block(subsection_key, block_data)
                 contains_gated_content = include_access and block_data.get_xblock_field(
