50ab6ad36250146e69ce601bbb5822181330ab43
Chandana3008 <chandana.k@talentsprint.com>
2024-10-14 12:27:39 +0530

    Debugging

--- common/djangoapps/student/views/management.py
+++ common/djangoapps/student/views/management.py
@@ -1176,7 +1176,7 @@ def get_zoom_link(meeting_id, webinar_id, data):
         log.error(zoom_data)
         
         
-        zoom_token = _get_zoom_auth_token() if not useLens else "Basic {0}".format(base64.b64encode("ts_c83ae02c3213115a:f7cdfc2017ebdd4c19a45702db789632"))
+        zoom_token = _get_zoom_auth_token() if not useLens else "Basic {0}".format(base64.encode("ts_c83ae02c3213115a:f7cdfc2017ebdd4c19a45702db789632"))
         if zoom_token:
             headers = {"Authorization" : zoom_token, "Content-Type" : "application/json"}
             response = requests.post(zoom_url, data=json.dumps(zoom_data), headers=headers)
