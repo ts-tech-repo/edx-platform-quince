f4a34b151d84a17615201c198fcf4e7173927713
sabid.a <sabid.a@talentsprint.com>
2024-11-01 15:35:56 +0530

    Debug: Added letter_grade field

--- lms/djangoapps/course_home_api/progress/serializers.py
+++ lms/djangoapps/course_home_api/progress/serializers.py
@@ -31,6 +31,7 @@ class SubsectionScoresSerializer(ReadOnlySerializer):
     learner_has_access = serializers.SerializerMethodField()
     num_points_earned = serializers.FloatField(source='graded_total.earned')
     num_points_possible = serializers.FloatField(source='graded_total.possible')
+    letter_grade = serializers.CharField()
     percent_graded = serializers.FloatField()
     problem_scores = serializers.SerializerMethodField()
     show_correctness = serializers.CharField()
