e6937db24abfe182840319fb0131c75db30df604
sabid.a <sabid.a@talentsprint.com>
2024-11-01 14:45:37 +0530

    Debug: Added letter_grade field

--- lms/djangoapps/grades/subsection_grade.py
+++ lms/djangoapps/grades/subsection_grade.py
@@ -396,7 +396,7 @@ class CreateSubsectionGrade(NonZeroSubsectionGrade):
             possible_graded=self.graded_total.possible,
             visible_blocks=self._get_visible_blocks,
             first_attempted=self.all_total.first_attempted,
-            letter_grade=self.problem_scores.get('letter_grade', ''),
+            letter_grade='B',
         )
 
     @property
