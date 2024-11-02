66841cf6e43ad58018c462c3247306961cbae007
sabid.a <sabid.a@talentsprint.com>
2024-11-03 00:30:02 +0530

    Debug: Added letter_grade field

--- lms/djangoapps/grades/subsection_grade.py
+++ lms/djangoapps/grades/subsection_grade.py
@@ -44,7 +44,7 @@ class SubsectionGradeBase(metaclass=ABCMeta):
 
         log.info("#sabidA #v7 subsection: %s", subsection)
         log.info("#sabidA #v7.1 subsection: %s", str(subsection))
-        self.letter_grade = subsection.get('letter_grade', 'TEST')
+        self.letter_grade = getattr(subsection, 'letter_grade', 'TEST')
 
     @property
     def letter_grade(self):
