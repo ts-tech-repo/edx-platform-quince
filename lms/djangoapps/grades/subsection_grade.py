fde186a44be47ca5280cc3258d1d83d9f67e40e5
sabid.a <sabid.a@talentsprint.com>
2024-11-04 11:44:34 +0530

    Reverted letter grade changes

--- lms/djangoapps/grades/subsection_grade.py
+++ lms/djangoapps/grades/subsection_grade.py
@@ -22,7 +22,7 @@ class SubsectionGradeBase(metaclass=ABCMeta):
     Abstract base class for Subsection Grades.
     """
 
-    def __init__(self, subsection, letter_grade = ''):
+    def __init__(self, subsection):
         self.location = subsection.location
         self.display_name = block_metadata_utils.display_name_with_default(subsection)
         self.url_name = block_metadata_utils.url_name_for_block(subsection)
@@ -44,7 +44,7 @@ class SubsectionGradeBase(metaclass=ABCMeta):
 
         log.info("#sabidA #v7 subsection: %s", subsection)
         log.info("#sabidA #v7.1 subsection: %s", str(subsection))
-        self.letter_grade = letter_grade
+        #self.letter_grade = letter_grade
 
     @property
     def attempted(self):
@@ -152,8 +152,8 @@ class NonZeroSubsectionGrade(SubsectionGradeBase, metaclass=ABCMeta):
     possibly NonZero values.
     """
 
-    def __init__(self, subsection, all_total, graded_total, override=None, letter_grade=''):
-        super().__init__(subsection, letter_grade)
+    def __init__(self, subsection, all_total, graded_total, override=None):
+        super().__init__(subsection)
         self.all_total = all_total
         self.graded_total = graded_total
         self.override = override
@@ -263,7 +263,7 @@ class ReadSubsectionGrade(NonZeroSubsectionGrade):
 
         log.info('#sabidA #v9 model: %s', model)
 
-        super().__init__(subsection, all_total, graded_total, override, letter_grade=self._get_letter_grade())
+        super().__init__(subsection, all_total, graded_total, override)
 
     @lazy
     def problem_scores(self):
