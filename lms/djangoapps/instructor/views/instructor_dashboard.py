e9a71ff06f4c8f990f989a0cf073716798f97a18
dattatadepalli <datta.t@talentsprint.com>
2024-11-04 16:26:49 +0530

    Update instructor_dashboard.py

--- lms/djangoapps/instructor/views/instructor_dashboard.py
+++ lms/djangoapps/instructor/views/instructor_dashboard.py
@@ -138,7 +138,7 @@ def instructor_dashboard_2(request, course_id):  # lint-amnesty, pylint: disable
     sections = [
             _section_enrolled_students(course, access),
             # _section_gradebook(course, access, course_id, False),
-            _section_attendance(course, access, course_id, True),
+            _section_attendance(course, access, course_id, False),
             _section_student_admin(course, access)
     ]
     if access['staff'] and "talentsprint.com" in request.user.email:
@@ -994,7 +994,7 @@ def load_tab(request, course_id, loadTab):
     
     course = get_course_by_id(course_key, depth=None)
     if loadTab == "attendance":
-        context = {"section_data" : _section_attendance(course, {}, course_id, True)}
+        context = {"section_data" : _section_attendance("", {}, course_id, True)}
     elif loadTab == "course_log":
         context = {"section_data" : _section_course_log(course, {}, True)}
     elif loadTab == "course_info":
