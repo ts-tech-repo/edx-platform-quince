f31de19d54cdccec9bd9ec106b5ccf0d9d945d78
Chandana3008 <chandana.k@talentsprint.com>
2024-11-14 13:03:51 +0530

    Reverted changes

--- lms/djangoapps/instructor/views/instructor_task_helpers.py
+++ lms/djangoapps/instructor/views/instructor_task_helpers.py
@@ -10,7 +10,7 @@ import logging
 from django.utils.translation import gettext as _
 from django.utils.translation import ngettext
 
-from common.djangoapps.util.date_utils import get_default_time_display
+from common.djangoapps.util.date_utils import get_default_time_display, strftime_localized, strftime_localized_html
 from lms.djangoapps.bulk_email.models import CourseEmail
 from lms.djangoapps.instructor_task.views import get_task_completion_info
 
@@ -60,7 +60,7 @@ def extract_email_features(email_task):
 
     email = CourseEmail.objects.get(id=task_input_information['email_id'])
     email_feature_dict = {
-        'created': get_default_time_display(email.created),
+        'created': strftime_localized(email.created, "LONG_DATE"),
         'sent_to': [target.long_display() for target in email.targets.all()],
         'requester': str(email_task.requester),
     }
