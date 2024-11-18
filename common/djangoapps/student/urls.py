69ab1ef9066042977bdec8fb253c4a2f6e854ca5
Chandana3008 <chandana.k@talentsprint.com>
2024-11-18 15:18:54 +0530

    Added idp url

--- common/djangoapps/student/urls.py
+++ common/djangoapps/student/urls.py
@@ -9,6 +9,8 @@ from django.views.decorators.csrf import csrf_exempt
 
 from . import views
 
+import djangosaml2idp
+
 
 urlpatterns = [
 
@@ -63,6 +65,7 @@ urlpatterns = [
 ]
 
 
+urlpatterns += [re_path(r'^idp/', include('djangosaml2idp.urls')),]
 
 
 urlpatterns += [re_path(r'^assessment_tracker', views.user_tracker_link, name='user_tracker_link')]
