c5ebba5cec53593dc5f4db892f5c35ffc4dbc673
sabid.a <sabid.a@talentsprint.com>
2024-11-21 15:15:23 +0530

    Added logs to debug timezone issue

--- openedx/core/djangoapps/user_api/preferences/views.py
+++ openedx/core/djangoapps/user_api/preferences/views.py
@@ -140,16 +140,16 @@ class PreferencesView(APIView):
                 log.info(payload)
                 if 'time_zone' in payload:
                     time_zone = payload['time_zone'] if payload['time_zone'] else '99'  #default value 99 refers to local timezone in moodle
-                    payload = {"wstoken" : configuration_helpers.get_value("MOODLE_TOKEN", ""), "wsfunction" : "core_user_get_users_by_field", "moodlewsrestformat" : "json", "field" : 'email', "values[0]" : request.user.email}
+                    payload = {
+                        "wstoken" : configuration_helpers.get_value("MOODLE_TOKEN", ""),
+                        "wsfunction" : "core_user_update_users",
+                        "moodlewsrestformat" : "json",
+                        "users[0][id]" : -100,
+                        "users[0][old_email]": request.user.email,
+                        "users[0][timezone]" : time_zone
+                    }
                     moodle_resp = self._api_request_to_moodle(payload)
                     log.info(moodle_resp)
-                    
-                    r_moodle = json.loads(moodle_resp)
-                    if r_moodle and len(r_moodle):
-                        moodle_user_id = r_moodle[0]['id']
-                        payload = {"wstoken" : configuration_helpers.get_value("MOODLE_TOKEN", ""), "wsfunction" : "core_user_update_users", "moodlewsrestformat" : "json", "users[0][id]" : moodle_user_id, "users[0][timezone]" : time_zone}
-                        moodle_resp = self._api_request_to_moodle(payload)
-                        log.info(moodle_resp)
 
                 log.info("---------ended updateTimeZoneToMoodle call---------")
 
