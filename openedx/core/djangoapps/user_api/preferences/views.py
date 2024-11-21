26c6c11d801f09f71a6bf54fac72395793e9d078
sabid.a <sabid.a@talentsprint.com>
2024-11-21 15:35:45 +0530

    Added logs to debug timezone issue

--- openedx/core/djangoapps/user_api/preferences/views.py
+++ openedx/core/djangoapps/user_api/preferences/views.py
@@ -135,23 +135,22 @@ class PreferencesView(APIView):
                 update_user_preferences(request.user, request.data, user=username)
                 
                 #SA || updateTimeZoneToMoodle
-                payload = request.data
-                log.info("---------started updateTimeZoneToMoodle call---------")
-                log.info(payload)
-                if 'time_zone' in payload:
-                    time_zone = payload['time_zone'] if payload['time_zone'] else '99'  #default value 99 refers to local timezone in moodle
-                    payload = {
-                        "wstoken" : configuration_helpers.get_value("MOODLE_TOKEN", ""),
-                        "wsfunction" : "core_user_update_users",
-                        "moodlewsrestformat" : "json",
-                        "users[0][id]" : -100,
-                        "users[0][old_email]": request.user.email,
-                        "users[0][timezone]" : time_zone
-                    }
-                    moodle_resp = self._api_request_to_moodle(payload)
-                    log.info(moodle_resp)
-
-                log.info("---------ended updateTimeZoneToMoodle call---------")
+                try:
+                    payload = request.data
+                    if 'time_zone' in payload:
+                        time_zone = payload['time_zone'] if payload['time_zone'] else '99'  #default value 99 refers to local timezone in moodle
+                        payload = {
+                            "wstoken" : configuration_helpers.get_value("MOODLE_TOKEN", ""),
+                            "wsfunction" : "core_user_update_users",
+                            "moodlewsrestformat" : "json",
+                            "users[0][id]" : -100,
+                            "users[0][old_email]": request.user.email,
+                            "users[0][timezone]" : time_zone
+                        }
+                        moodle_resp = self._api_request_to_moodle(payload)
+                        log.info(moodle_resp)
+                except Exception as e:
+                    log.error(e)
 
         except UserNotAuthorized:
             return Response(status=status.HTTP_403_FORBIDDEN)
