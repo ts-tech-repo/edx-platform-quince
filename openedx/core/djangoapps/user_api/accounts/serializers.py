0afa5835a3b65dc795a37bea837df2eb8e23fa13
manoj.p3 <manoj.p@talentsprint.com>
2024-11-26 16:53:53 +0530

    Added company in serilizers

--- openedx/core/djangoapps/user_api/accounts/serializers.py
+++ openedx/core/djangoapps/user_api/accounts/serializers.py
@@ -287,7 +287,7 @@ class AccountLegacyProfileSerializer(serializers.HyperlinkedModelSerializer, Rea
     class Meta:
         model = UserProfile
         fields = (
-            "name", "gender", "goals", "year_of_birth", "level_of_education", "country", "state", "social_links",
+            "name", "gender", "goals", "year_of_birth", "level_of_education", "company", "country", "state", "social_links",
             "mailing_address", "bio", "profile_image", "requires_parental_consent", "language_proficiencies",
             "phone_number", "city"
         )
