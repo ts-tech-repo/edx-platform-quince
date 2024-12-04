7eaa4fc4a930cb5c2145fc0167489239fb2d1938
venkata-s123 <venkata.s@talentsprint.com>
2024-12-04 16:18:01 +0530

    added log

--- lms/djangoapps/grades/rest_api/v1/gradebook_views.py
+++ lms/djangoapps/grades/rest_api/v1/gradebook_views.py
@@ -535,6 +535,7 @@ class GradebookView(GradeViewMixin, PaginatedAPIView):
                 return mode == CourseMode.MASTERS
 
         #if is_masters_student():
+        log.info("#venkat profile::: %s",user.profile)
         user_entry['full_name'] = user.profile.firstname
         log.info("#venkat fullname::: %s",user_entry['full_name'])
         external_user_key = get_external_key_by_user_and_course(user, course.id)
