9997a84c6030942d47dc85885106ee9d5e8db31a
Chandana3008 <74036324+Chandana3008@users.noreply.github.com>
2024-12-05 13:51:20 +0530

    Debugging

--- lms/djangoapps/instructor/views/course_log.py
+++ lms/djangoapps/instructor/views/course_log.py
@@ -59,7 +59,7 @@ def get_course_unit_log_analytics(course_id):
 				subsection_name = get_subsection_name(i["block_id"], course_structure)
 				section_name = get_section_name(subsection_name[1], course_structure)
                 unit_type = i["fields"]["children"][0][0] if i["fields"]["children"] else ""
-                if unit_type != video:
+                if unit_type != "video":
                     continue
 				data.append({"unit_name" : i["fields"]["display_name"], "edited_by_email" : u.email, "edited_by_name" : u.first_name, "edited_on" : i["edit_info"]["edited_on"], "subsection_name": subsection_name[0],"section_name" : section_name, "block_type": i["block_type"], "block_id": i["block_id"], "unit_type": unit_type})
 
