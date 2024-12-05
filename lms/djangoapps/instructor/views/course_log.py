3c241f38ce87ab8eb8626891a7522b786a174f15
Chandana3008 <74036324+Chandana3008@users.noreply.github.com>
2024-12-05 13:56:17 +0530

    Issue fixes

--- lms/djangoapps/instructor/views/course_log.py
+++ lms/djangoapps/instructor/views/course_log.py
@@ -63,10 +63,10 @@ def get_course_unit_log_analytics(course_id):
                     continue
                 data.append({"unit_name" : i["fields"]["display_name"], "edited_by_email" : u.email, "edited_by_name" : u.first_name, "edited_on" : i["edit_info"]["edited_on"], "subsection_name": subsection_name[0],"section_name" : section_name, "block_type": i["block_type"], "block_id": i["block_id"], "unit_type": unit_type})
 
-		course_log = sorted(data, key = lambda k:k['edited_on'], reverse=True)
-		#course_log = json.loads(dumps(course_log))
-	except Exception as e:
-		log.error(e)
+        course_log = sorted(data, key = lambda k:k['edited_on'], reverse=True)
+        #course_log = json.loads(dumps(course_log))
+    except Exception as e:
+        log.error(e)
 
 	return {"course_log" : course_log}
 
