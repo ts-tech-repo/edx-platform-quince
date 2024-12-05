e8d14bea798acf7c49b3801e27079ad5943362af
venkata-s123 <154975888+venkata-s123@users.noreply.github.com>
2024-12-05 15:19:28 +0530

    Added frist name in gradebook

--- lms/djangoapps/grades/rest_api/v1/gradebook_views.py
+++ lms/djangoapps/grades/rest_api/v1/gradebook_views.py
@@ -523,7 +523,7 @@ class GradebookView(GradeViewMixin, PaginatedAPIView):
                 return mode == CourseMode.MASTERS
 
         #if is_masters_student():
-        user_entry['full_name'] = user.profile.name
+        user_entry['full_name'] = user.first_name
 
         external_user_key = get_external_key_by_user_and_course(user, course.id)
         if external_user_key:
