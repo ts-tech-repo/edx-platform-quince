5b14a710f139d91d03b116207a65ff3ab6a05559
sohail.m <sohail.m@talentsprint.com>
2024-12-05 10:42:24 +0530

    added py script for speed control

--- xmodule/video_block/transcripts_utils.py
+++ xmodule/video_block/transcripts_utils.py
@@ -499,8 +499,8 @@ def youtube_speed_dict(item):
     """
     Returns {speed: youtube_ids, ...} dict for existing youtube_ids
     """
-    yt_ids = [item.youtube_id_0_75, item.youtube_id_1_0, item.youtube_id_1_25, item.youtube_id_1_5]
-    yt_speeds = [0.75, 1.00, 1.25, 1.50]
+    yt_ids = [item.youtube_id_0_75, item.youtube_id_1_0, item.youtube_id_1_25, item.youtube_id_1_5, item.youtube_id_1_75]
+    yt_speeds = [0.75, 1.00, 1.25, 1.50, 1.75]
     youtube_ids = {p[0]: p[1] for p in zip(yt_ids, yt_speeds) if p[0]}
     return youtube_ids
 
