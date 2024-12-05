41eb2f523a2b292b10ef45df45a9f2fa4e38d1a2
Aman-K-Ray <aman.k@talentsprint.com>
2024-12-05 15:19:50 +0530

    added log

--- cms/static/js/views/utils/xblock_utils.js
+++ cms/static/js/views/utils/xblock_utils.js
@@ -299,6 +299,7 @@ function($, _, gettext, ViewUtils, ModuleUtils, XBlockInfo, StringUtils) {
         return ViewUtils.runOperationShowingMessage(gettext('Saving'),
             function() {
                 xblockInfo.save(xblockData, options);
+                console.log("Aman updateXBlockFields");
                 return xblockInfo.save(xblockData, options);
             }
         );
