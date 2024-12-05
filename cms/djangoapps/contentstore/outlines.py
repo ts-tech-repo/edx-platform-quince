02f8e454dfaa9b1a7ec607559d436390068a4ed2
Aman-K-Ray <aman.k@talentsprint.com>
2024-12-05 10:21:31 +0530

    debugging

--- cms/djangoapps/contentstore/outlines.py
+++ cms/djangoapps/contentstore/outlines.py
@@ -336,7 +336,6 @@ def get_outline_from_modulestore(course_key) -> Tuple[CourseOutlineData, List[Co
 
     with store.branch_setting(ModuleStoreEnum.Branch.published_only, course_key):
         # Pull course with depth=3 so we prefetch Section -> Sequence -> Unit
-        log.info("#AMANK:::: Getting course %s from modulestore ", course_key)
         log.info("#AMANK:::: branch_setting: %s ", ModuleStoreEnum.Branch.published_only)
         log.info("#AMANK:::: store: %s ", store)
         course = store.get_course(course_key, depth=3)
--- xmodule/modulestore/mixed.py
+++ xmodule/modulestore/mixed.py
@@ -205,8 +205,10 @@ class MixedModuleStore(ModuleStoreDraftAndPublished, ModuleStoreWriteBase):
         """
         if locator is not None:
             locator = self._clean_locator_for_mapping(locator)
+            log.info("#AMANK #7 Getting modulestore for locator: %s", locator)
             mapping = self.mappings.get(locator, None)
             if mapping is not None:
+                log.info("#AMANK #7 Found existing mapping for %s: %s", locator, mapping)
                 return mapping
             else:
                 if isinstance(locator, LibraryLocator):
@@ -215,10 +217,13 @@ class MixedModuleStore(ModuleStoreDraftAndPublished, ModuleStoreWriteBase):
                     has_locator = lambda store: store.has_course(locator)
                 for store in self.modulestores:
                     if has_locator(store):
+                        log.info("#AMANK #7 Adding mapping for %s: %s", locator, store)
+                        log.info("#AMANK #7 self.mappings: %s", self.mappings)
                         self.mappings[locator] = store
                         return store
 
         # return the default store
+        log.info("#AMANK #7 Returning default modulestore: %s", self.default_modulestore)
         return self.default_modulestore
 
     def _get_modulestore_by_type(self, modulestore_type):
