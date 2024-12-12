1e50cdbdae517c6e6f29851de85c329a219f272d
venkata-s123 <venkata.s@talentsprint.com>
2024-12-12 13:03:57 +0530

    added log

--- lms/djangoapps/grades/rest_api/v1/gradebook_views.py
+++ lms/djangoapps/grades/rest_api/v1/gradebook_views.py
@@ -556,7 +556,7 @@ class GradebookView(GradeViewMixin, PaginatedAPIView):
         """
        
         course = get_course_by_id(course_key, depth=None)
-        log.info("venkat coursetotal:", course)
+        log.info("venkat coursetotal: %s", course)
         # We fetch the entire course structure up-front, and use this when iterating
         # over users to determine their subsection grades.  We purposely avoid fetching
         # the user-specific course structure for each user, because that is very expensive.
