41443cefdd33d142fe15b0a7e709adc88d00c1a8
venkata-s123 <venkata.s@talentsprint.com>
2024-12-17 15:03:42 +0530

    added logs for course block

--- lms/djangoapps/course_home_api/outline/views.py
+++ lms/djangoapps/course_home_api/outline/views.py
@@ -2,7 +2,7 @@
 Outline Tab Views
 """
 from datetime import datetime, timezone
-
+import logging
 from completion.exceptions import UnavailableCompletionData  # lint-amnesty, pylint: disable=wrong-import-order
 from completion.utilities import get_key_to_last_completed_block  # lint-amnesty, pylint: disable=wrong-import-order
 from django.conf import settings  # lint-amnesty, pylint: disable=wrong-import-order
@@ -53,6 +53,7 @@ from openedx.features.course_experience.url_helpers import get_learning_mfe_home
 from openedx.features.course_experience.utils import get_course_outline_block_tree, get_start_block
 from openedx.features.discounts.utils import generate_offer_data
 from xmodule.course_block import COURSE_VISIBILITY_PUBLIC, COURSE_VISIBILITY_PUBLIC_OUTLINE  # lint-amnesty, pylint: disable=wrong-import-order
+log = logging.getLogger(__name__)
 
 
 class UnableToDismissWelcomeMessage(APIException):
@@ -304,7 +305,7 @@ class OutlineTabView(RetrieveAPIView):
             available_seq_ids = {str(usage_key) for usage_key in user_course_outline.sequences}
 
             available_section_ids = {str(section.usage_key) for section in user_course_outline.sections}
-
+            log.info('#venkat courseblock %s',course_blocks)
             # course_blocks is a reference to the root of the course,if chapter_data['id'] in available_section_ids
             # so we go through the chapters (sections) and keep only those
             # which are part of the outline.
