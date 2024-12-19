9dee8ddd164428cba1d5ea877ed10c44c44070ab
venkata-s123 <venkata.s@talentsprint.com>
2024-12-19 11:00:21 +0530

    commented the code

--- cms/static/js/views/course_outline.js
+++ cms/static/js/views/course_outline.js
@@ -94,9 +94,9 @@ function(
                 //  - make its name editable
                 this.refresh(this.createNewItemViewState(locator, ViewUtils.getScrollOffset($(event.target))));
             }
-            XBlockViewUtils.updateXBlockFields(this.model, {"publish":"make_public"}, {
-                success: this.options.onSave
-            });
+            // XBlockViewUtils.updateXBlockFields(this.model, {"publish":"make_public"}, {
+            //     success: this.options.onSave
+            // });
         },
 
         /**
@@ -110,9 +110,6 @@ function(
             console.log("X block type 1",xblockType);
             if (xblockType === 'section') {
                 this.onSectionAdded(locator, xblockElement, scrollOffset);
-                XBlockViewUtils.updateXBlockFields(this.model, {"publish":"make_public"}, {
-                        success: this.options.onSave
-                    });
             } else {
                 // For all other block types, refresh the view and do the following:
                 //  - show the new block expanded
