3766a50a2c10b35c9f199df4d28cd2450c81b61e
venkata-s123 <venkata.s@talentsprint.com>
2024-12-26 16:42:16 +0530

    added logs

--- lms/djangoapps/grades/rest_api/v1/gradebook_views.py
+++ lms/djangoapps/grades/rest_api/v1/gradebook_views.py
@@ -685,7 +685,7 @@ class GradebookView(GradeViewMixin, PaginatedAPIView):
                     if not exc:
                         entry = self._gradebook_entry(user, course, graded_subsections, course_grade)
                         user_names = CourseEnrollment.objects.filter(course__id=entry.get("course_id",""), user_id = entry.get("user_id",""),is_active=True)
-                        log.info("#venkat usernames::: %s",user_names)
+                        log.info("#venkat usernames::: %s",vars(user_names))
                         if '@ts.com' in entry["email"] or '@talentsprint.com' in entry["email"]:
                             continue
                         log.info("#venkat entrydata::: %s",entry)
