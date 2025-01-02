2524e949bada73359948c4ce1982e7704e0bcf67
venkata-s123 <venkata.s@talentsprint.com>
2025-01-02 17:57:10 +0530

    redirect to login page

--- cms/djangoapps/contentstore/views/error.py
+++ cms/djangoapps/contentstore/views/error.py
@@ -50,6 +50,5 @@ def render_404(request, exception):  # lint-amnesty, pylint: disable=unused-argu
 @fix_crum_request
 @jsonable_error(500, "The Studio servers encountered an error")
 def render_500(request):
-    log.info("#venkat testing1334")
-    return redirect("https://staging.quince02.talentsprint.com/authn/login")
+    return redirect("/")
     # return HttpResponseServerError(render_to_string('500.html', {}, request=request))
