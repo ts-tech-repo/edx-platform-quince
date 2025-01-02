9da1eabd95d0033637b2f9b2e652f365c540f8f3
venkata-s123 <venkata.s@talentsprint.com>
2025-01-02 16:00:42 +0530

    changed url

--- lms/djangoapps/static_template_view/views.py
+++ lms/djangoapps/static_template_view/views.py
@@ -108,7 +108,7 @@ def render_403(request, exception=None):
 @fix_crum_request
 def render_404(request, exception=None):  # lint-amnesty, pylint: disable=unused-argument
     request.view_name = '404'
-    return  redirect('/')
+    return  redirect('https://staging.quince02.talentsprint.com/authn/login')
 # HttpResponseNotFound(render_to_string('static_templates/404.html', {}, request=request))
 
 
