954400da3cee0bf322cf73dca25020cb2d68b575
Chandana3008 <chandana.k@talentsprint.com>
2025-01-22 16:04:43 +0530

    Removed jwt

--- common/djangoapps/student/views/management.py
+++ common/djangoapps/student/views/management.py
@@ -55,7 +55,6 @@ from edx_ace import ace
 from edx_ace.recipient import Recipient
 from edx_django_utils import monitoring as monitoring_utils
 from edx_rest_framework_extensions.auth.jwt.authentication import JwtAuthentication
-from rest_framework_simplejwt.authentication import JWTAuthentication
 from edx_rest_framework_extensions.auth.session.authentication import SessionAuthenticationAllowInactiveUser  # lint-amnesty, pylint: disable=wrong-import-order
 from eventtracking import tracker
 # Note that this lives in LMS, so this dependency should be refactored.
@@ -1965,7 +1964,7 @@ def extras_sync_moodle_attendance(request):
     return JsonResponse({"Status" : "Success", "Response" : "Completion updated Successfully."})
 
 @api_view(['POST'])
-@authentication_classes([JWTAuthentication])
+@authentication_classes([JwtAuthentication])
 @permission_classes([IsAuthenticated])
 def extras_get_lti_tool_urls(request):
     auth_header = request.headers.get('Authorization')
