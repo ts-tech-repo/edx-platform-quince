e454bec4a728777f456417ad4404369be14fbddf
Aman-K-Ray <aman.k@talentsprint.com>
2025-01-27 17:02:32 +0530

    added logs

--- lms/djangoapps/learner_home/views.py
+++ lms/djangoapps/learner_home/views.py
@@ -558,18 +558,22 @@ class InitializeView(APIView):  # pylint: disable=unused-argument
         ptc_popup_details = configuration_helpers.get_value("PTC_POPUP_DETAILS", None)
         if ptc_popup_details:
             try:
+                    logger.info(f"#AMANK:: PTC Popup: {ptc_popup_details}")
                     response = requests.get(
                         ptc_popup_details["PTC_API_URL"],
                         data={"batchId":  ptc_popup_details["PTC_BATCH_ID"], "emailId": user.email},
                         headers={"Access-Key": ptc_popup_details["PTC_API_ACCESS_KEY"]},
                     )
+                    logger.info(f"#AMANK:: PTC Response: {response.json()}")
                     parsed_data = response.json()
-                    if parsed_data["status"] == "success" and parsed_data["data"]["ptcStatus"]:
+                    if parsed_data["status"] == "success" and parsed_data["data"]["ptcStatus"] == False:
+                        logger.info(f"#AMANK:: PTC status: {parsed_data['data']['ptcStatus']}")
+                        learner_dash_data["ptcSubmitted"] = False
                         emailId = urllib.parse.quote(user.email)
                         learner_dash_data["ptcURl "]= f"{parsed_data["data"]["ptcURL"]}?emailId={emailId}"
+                    logger.info(f"#AMANK:: PTC URL: {learner_dash_data['ptcURl']}")
             except Exception as ex:
-                    learner_dash_data["ptcSubmitted"] = False
-
+                    logger.error(f"#AMANK:: PTC Exception: {ex}")
         context = {
             "audit_access_deadlines": audit_access_deadlines,
             "ecommerce_payment_page": ecommerce_payment_page,
