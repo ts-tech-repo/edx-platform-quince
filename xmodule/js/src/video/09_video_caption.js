ad05295eb608eb54f50866d13a77f29b1f8d4faf
sohail.m <sohail.m@talentsprint.com>
2025-02-05 16:24:08 +0530

    reverted changes

--- xmodule/js/src/video/09_video_caption.js
+++ xmodule/js/src/video/09_video_caption.js
@@ -86,8 +86,6 @@
                     HtmlUtils.HTML(
                         [
                             '<div class="grouped-controls">',
-                            '<i class="fa fa-cc" aria-hidden="true"></i>',
-                            '<li class="cc-settings"></li>',
                             '<button class="control toggle-captions" aria-disabled="false">',
                             '<span class="icon fa fa-cc" aria-hidden="true"></span>',
                             '</button>',
