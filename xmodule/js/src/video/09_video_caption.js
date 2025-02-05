1d1392e2ed57fd7b028c9e0f9f59110199cb2fde
sohail.m <sohail.m@talentsprint.com>
2025-02-05 16:03:50 +0530

    added tags for cc options

--- xmodule/js/src/video/09_video_caption.js
+++ xmodule/js/src/video/09_video_caption.js
@@ -85,7 +85,9 @@
                 var langHtml = HtmlUtils.interpolateHtml(
                     HtmlUtils.HTML(
                         [
-                            '<div class="grouped-controls test">',
+                            '<div class="grouped-controls">',
+                            '<i class="fa fa-cc" aria-hidden="true"></i>',
+                            '<li class="cc-settings"></li>',
                             '<button class="control toggle-captions" aria-disabled="false">',
                             '<span class="icon fa fa-cc" aria-hidden="true"></span>',
                             '</button>',
